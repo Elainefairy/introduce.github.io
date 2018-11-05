177ed8b8f4a0a44fb6432b208995d79fc0c1db58
Elaine Li <1812546797@qq.com>
2018-11-05 20:49:57 +0800

    modify

---
+++ 冒泡排序.js
@@ -0,0 +1,251 @@
+(function (cjs, an) {
+
+var p; // shortcut to reference prototypes
+var lib={};var ss={};var img={};
+lib.ssMetadata = [
+		{name:"冒泡排序_atlas_", frames: [[0,0,450,450],[904,452,450,450],[1356,452,450,450],[904,904,450,450],[904,1356,450,450],[1356,904,450,450],[1356,1356,450,450],[0,1356,450,450],[452,0,450,450],[904,0,450,450],[1356,0,450,450],[452,452,450,450],[452,904,450,450],[452,1356,450,450],[0,452,450,450],[0,904,450,450]]}
+];
+
+
+// symbols:
+
+
+
+(lib._1 = function() {
+	this.spriteSheet = ss["冒泡排序_atlas_"];
+	this.gotoAndStop(0);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._10 = function() {
+	this.spriteSheet = ss["冒泡排序_atlas_"];
+	this.gotoAndStop(1);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._11 = function() {
+	this.spriteSheet = ss["冒泡排序_atlas_"];
+	this.gotoAndStop(2);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._12 = function() {
+	this.spriteSheet = ss["冒泡排序_atlas_"];
+	this.gotoAndStop(3);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._13 = function() {
+	this.spriteSheet = ss["冒泡排序_atlas_"];
+	this.gotoAndStop(4);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._14 = function() {
+	this.spriteSheet = ss["冒泡排序_atlas_"];
+	this.gotoAndStop(5);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._15 = function() {
+	this.spriteSheet = ss["冒泡排序_atlas_"];
+	this.gotoAndStop(6);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._16 = function() {
+	this.spriteSheet = ss["冒泡排序_atlas_"];
+	this.gotoAndStop(7);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._2 = function() {
+	this.spriteSheet = ss["冒泡排序_atlas_"];
+	this.gotoAndStop(8);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._3 = function() {
+	this.spriteSheet = ss["冒泡排序_atlas_"];
+	this.gotoAndStop(9);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._4 = function() {
+	this.spriteSheet = ss["冒泡排序_atlas_"];
+	this.gotoAndStop(10);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._5 = function() {
+	this.spriteSheet = ss["冒泡排序_atlas_"];
+	this.gotoAndStop(11);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._6 = function() {
+	this.spriteSheet = ss["冒泡排序_atlas_"];
+	this.gotoAndStop(12);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._7 = function() {
+	this.spriteSheet = ss["冒泡排序_atlas_"];
+	this.gotoAndStop(13);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._8 = function() {
+	this.spriteSheet = ss["冒泡排序_atlas_"];
+	this.gotoAndStop(14);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._9 = function() {
+	this.spriteSheet = ss["冒泡排序_atlas_"];
+	this.gotoAndStop(15);
+}).prototype = p = new cjs.Sprite();
+
+
+
+// stage content:
+(lib.恢复_无标题1_20181014155112 = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+	// 图层_1
+	this.instance = new lib._1();
+	this.instance.parent = this;
+
+	this.instance_1 = new lib._2();
+	this.instance_1.parent = this;
+
+	this.instance_2 = new lib._3();
+	this.instance_2.parent = this;
+
+	this.instance_3 = new lib._4();
+	this.instance_3.parent = this;
+
+	this.instance_4 = new lib._5();
+	this.instance_4.parent = this;
+
+	this.instance_5 = new lib._6();
+	this.instance_5.parent = this;
+
+	this.instance_6 = new lib._7();
+	this.instance_6.parent = this;
+
+	this.instance_7 = new lib._8();
+	this.instance_7.parent = this;
+
+	this.instance_8 = new lib._9();
+	this.instance_8.parent = this;
+
+	this.instance_9 = new lib._10();
+	this.instance_9.parent = this;
+
+	this.instance_10 = new lib._11();
+	this.instance_10.parent = this;
+
+	this.instance_11 = new lib._12();
+	this.instance_11.parent = this;
+
+	this.instance_12 = new lib._13();
+	this.instance_12.parent = this;
+
+	this.instance_13 = new lib._14();
+	this.instance_13.parent = this;
+
+	this.instance_14 = new lib._15();
+	this.instance_14.parent = this;
+
+	this.instance_15 = new lib._16();
+	this.instance_15.parent = this;
+
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.instance}]}).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_2}]},1).to({state:[{t:this.instance_3}]},1).to({state:[{t:this.instance_4}]},1).to({state:[{t:this.instance_5}]},1).to({state:[{t:this.instance_6}]},1).to({state:[{t:this.instance_7}]},1).to({state:[{t:this.instance_8}]},1).to({state:[{t:this.instance_9}]},1).to({state:[{t:this.instance_10}]},1).to({state:[{t:this.instance_11}]},1).to({state:[{t:this.instance_12}]},1).to({state:[{t:this.instance_13}]},1).to({state:[{t:this.instance_14}]},1).to({state:[{t:this.instance_15}]},1).wait(1));
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(225,225,450,450);
+// library properties:
+lib.properties = {
+	id: 'C4F5533357482F479676E467EFAD6863',
+	width: 450,
+	height: 450,
+	fps: 0.5,
+	color: "#FFFFFF",
+	opacity: 1.00,
+	manifest: [
+		{src:"images/冒泡排序_atlas_.png", id:"冒泡排序_atlas_"}
+	],
+	preloads: []
+};
+
+
+
+// bootstrap callback support:
+
+(lib.Stage = function(canvas) {
+	createjs.Stage.call(this, canvas);
+}).prototype = p = new createjs.Stage();
+
+p.setAutoPlay = function(autoPlay) {
+	this.tickEnabled = autoPlay;
+}
+p.play = function() { this.tickEnabled = true; this.getChildAt(0).gotoAndPlay(this.getTimelinePosition()) }
+p.stop = function(ms) { if(ms) this.seek(ms); this.tickEnabled = false; }
+p.seek = function(ms) { this.tickEnabled = true; this.getChildAt(0).gotoAndStop(lib.properties.fps * ms / 1000); }
+p.getDuration = function() { return this.getChildAt(0).totalFrames / lib.properties.fps * 1000; }
+
+p.getTimelinePosition = function() { return this.getChildAt(0).currentFrame / lib.properties.fps * 1000; }
+
+an.bootcompsLoaded = an.bootcompsLoaded || [];
+if(!an.bootstrapListeners) {
+	an.bootstrapListeners=[];
+}
+
+an.bootstrapCallback=function(fnCallback) {
+	an.bootstrapListeners.push(fnCallback);
+	if(an.bootcompsLoaded.length > 0) {
+		for(var i=0; i<an.bootcompsLoaded.length; ++i) {
+			fnCallback(an.bootcompsLoaded[i]);
+		}
+	}
+};
+
+an.compositions = an.compositions || {};
+an.compositions['C4F5533357482F479676E467EFAD6863'] = {
+	getStage: function() { return exportRoot.getStage(); },
+	getLibrary: function() { return lib; },
+	getSpriteSheet: function() { return ss; },
+	getImages: function() { return img; }
+};
+
+an.compositionLoaded = function(id) {
+	an.bootcompsLoaded.push(id);
+	for(var j=0; j<an.bootstrapListeners.length; j++) {
+		an.bootstrapListeners[j](id);
+	}
+}
+
+an.getComposition = function(id) {
+	return an.compositions[id];
+}
+
+
+
+})(createjs = createjs||{}, AdobeAn = AdobeAn||{});
+var createjs, AdobeAn;
